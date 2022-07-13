12084712ffc8d08cb7deb6154b870bc4448350a6
Hongchao Liu <lbs.lhc@gmail.com>
2022-07-13 10:16:40 +0200

    Add wallet commands

--- src/commands/alephium/wallets/balance.ts
+++ src/commands/alephium/wallets/balance.ts
@@ -1,25 +1,23 @@
 import { Command, Flags } from '@oclif/core'
 import { NodeProvider } from '@alephium/web3'
 
-export default class GetState extends Command {
-  static description = 'Get contract state'
+export default class Balance extends Command {
+  static description = 'Get the balance for a wallet'
 
   static examples = [
-    `$ oex alephium contract get-state 01010300010004144020a8ff18caf0ea2eb9c397d7a15c9271cde81f372fd3bc4aae41ff53514b382e3a170016000104`,
+    `$ oex alephium wallets balance wallet-name`,
   ]
 
-  static args = [{ name: 'address', description: 'Contract Address', required: true }]
-
+  static args = [{ name: 'walletName', description: 'Wallet Name', required: true }]
   static flags = {
     nodeUrl: Flags.string({ char: 'n', description: 'Node URL', required: false, default: "http://127.0.0.1:22973" }),
   }
 
   async run(): Promise<void> {
-    const { args, flags } = await this.parse(GetState)
-
+    const { args, flags } = await this.parse(Balance)
     const nodeProvider = new NodeProvider(flags.nodeUrl)
 
-    await nodeProvider.contracts.getContractsAddressState(args.address, { group: 0 })
+    await nodeProvider.wallets.getWalletsWalletNameBalances(args.walletName)
       .then(state => {
         this.log(JSON.stringify(state))
       })
--- src/commands/alephium/wallets/list.ts
+++ src/commands/alephium/wallets/list.ts
@@ -1,25 +1,24 @@
 import { Command, Flags } from '@oclif/core'
 import { NodeProvider } from '@alephium/web3'
 
-export default class GetState extends Command {
-  static description = 'Get contract state'
+export default class List extends Command {
+  static description = 'List all the wallets'
 
   static examples = [
-    `$ oex alephium contract get-state 01010300010004144020a8ff18caf0ea2eb9c397d7a15c9271cde81f372fd3bc4aae41ff53514b382e3a170016000104`,
+    `$ oex alephium wallets list`,
   ]
 
-  static args = [{ name: 'address', description: 'Contract Address', required: true }]
-
+  static args = []
   static flags = {
     nodeUrl: Flags.string({ char: 'n', description: 'Node URL', required: false, default: "http://127.0.0.1:22973" }),
   }
 
   async run(): Promise<void> {
-    const { args, flags } = await this.parse(GetState)
+    const { flags } = await this.parse(List)
 
     const nodeProvider = new NodeProvider(flags.nodeUrl)
 
-    await nodeProvider.contracts.getContractsAddressState(args.address, { group: 0 })
+    await nodeProvider.wallets.getWallets()
       .then(state => {
         this.log(JSON.stringify(state))
       })
