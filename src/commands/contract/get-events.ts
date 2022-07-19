a14573f9e5b24a69fca293f52421585a7306a323
h0ngcha0 <lbs.lhc@gmail.com>
2022-07-19 16:30:04 +0200

    Add get events

---
+++ src/commands/contract/get-events.ts
@@ -0,0 +1,50 @@
+import { NodeProvider, subscribeToEvents, SubscribeOptions } from '@alephium/web3'
+import { ContractEvent } from '@alephium/web3/dist/src/api/api-alephium'
+import { AlephiumCommand } from '../../common'
+import { Flags, CliUx } from '@oclif/core'
+
+export default class GetEvents extends AlephiumCommand {
+  static description = 'Get contract state'
+  static examples = [
+    '$ alephium-cli contract get-events 2Bhm8HSoxHRRAxY6RvoqWgMJZDBfpSZxyZr7MNkh6HGnC',
+  ]
+
+  static args = [{ name: 'address', description: 'Contract Address', required: true }]
+  static flags = {
+    nodeUrl: GetEvents.nodeUrlFlag,
+    streaming: Flags.boolean({
+      char: 's',
+      description: 'Stream contract events',
+      required: false,
+      default: false,
+    }),
+  }
+
+  async run(): Promise<void> {
+    const { args, flags } = await this.parse(GetEvents)
+    const nodeProvider = new NodeProvider(flags.nodeUrl)
+
+    if (flags.streaming) {
+      const subscriptOptions: SubscribeOptions<ContractEvent> = {
+        provider: nodeProvider,
+        pollingInterval: 500,
+        messageCallback: (event: ContractEvent): Promise<void> => {
+          console.log(event)
+          return Promise.resolve()
+        },
+        errorCallback: (error: any, subscription): Promise<void> => {
+          console.log(error)
+          subscription.unsubscribe()
+          return Promise.resolve()
+        },
+      }
+
+      CliUx.ux.action.start('Fetching events', 'polling', { stdout: true })
+      subscribeToEvents(subscriptOptions, args.address)
+    } else {
+      this.printApiResponse(
+        nodeProvider.events.getEventsContractContractaddress(args.address, { start: 0 }),
+      )
+    }
+  }
+}
