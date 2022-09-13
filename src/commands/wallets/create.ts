14646fd02fad91d1a13aa0171757ccf2a9b9987a
h0ngcha0 <lbs.lhc@gmail.com>
2022-09-13 14:47:13 +0200

    Create wallet

---
+++ src/commands/wallets/create.ts
@@ -0,0 +1,49 @@
+import { NodeProvider } from '@alephium/web3'
+import { Flags } from '@oclif/core'
+import { Command } from '../../common'
+
+export default class Create extends Command {
+  static description = 'Create a new wallets'
+  static flags = {
+    ...Command.flags,
+    name: Flags.string({
+      char: 'w',
+      description: 'Wallet name',
+      required: true
+    }),
+    password: Flags.string({
+      char: 'p',
+      description: 'Password',
+      required: true
+    }),
+    isMiner: Flags.boolean({
+      char: 'm',
+      description: 'Whether a miner wallet',
+      required: false
+    }),
+    mnemonicPassphrase: Flags.string({
+      char: 's',
+      description: 'Mnemonic passphrase',
+      required: false
+    }),
+    mnemonicSize: Flags.integer({
+      char: 'z',
+      description: 'Mnemonic size',
+      required: false
+    })
+  }
+
+  async run(): Promise<void> {
+    const { flags } = await this.parse(Create)
+    const nodeProvider = new NodeProvider(flags.nodeUrl)
+
+    const result = nodeProvider.wallets.postWallets({
+      "password": flags.password,
+      "walletName": flags.name,
+      "isMiner": flags.isMiner,
+      "mnemonicPassphrase": flags.mnemonicPassphrase,
+      "mnemonicSize": flags.mnemonicSize
+    })
+    await this.printApiResponse(result)
+  }
+}
